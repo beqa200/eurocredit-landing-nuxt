b7ecc3bcaf5fe1c271224e920e2cbd04a48f63e4
beqa <beqamaisuradze912@gmail.com>
2024-06-28 00:44:17 +0400

    install images

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<header>header</header>
+		<img src="/hero.png" />
+		<main>
+			<slot />
+		</main>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DefaultLayout",
+};
+</script>
+
+<style scoped>
+/* styles */
+</style>
