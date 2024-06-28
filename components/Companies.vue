94f04f471d0594a9fe2edde19d97c916fe4b6c08
beqa <beqamaisuradze912@gmail.com>
2024-06-28 15:31:09 +0400

    feat: add hero section

---
+++ components/Companies.vue
@@ -0,0 +1,30 @@
+<template>
+	<div
+		class="flex justify-center gap-12 opacity-40"
+	>
+		<NuxtImg
+			src="/boltshift-logo.png"
+			class="filter grayscale"
+		/>
+		<NuxtImg
+			src="lightbox-logo.png"
+			class="filter grayscale"
+		/>
+		<NuxtImg
+			src="featherdev-logo.png"
+			class="filter grayscale"
+		/>
+		<NuxtImg
+			src="spherule-logo.png"
+			class="filter grayscale"
+		/>
+		<NuxtImg
+			src="globalbank-logo.png"
+			class="filter grayscale"
+		/>
+		<NuxtImg
+			src="nietzsche-logo.png"
+			class="filter grayscale"
+		/>
+	</div>
+</template>
---
+++ components/HeroSection.vue
@@ -0,0 +1,50 @@
+<template>
+	<section
+		class="p-[114px] flex justify-between"
+	>
+		<div class="max-w-[500px]">
+			<h1 class="text-[56px] font-bold">
+				{{ $t("hero.title") }}
+			</h1>
+			<p
+				class="text-[18px] mt-[24px] mb-[40px]"
+			>
+				{{ $t("hero.description") }}
+			</p>
+			<div class="flex gap-2">
+				<button class="btn">
+					{{
+						$t("navigation.buttonText")
+					}}
+				</button>
+				<button
+					class="flex gap-2 items-center border-2 rounded-[88px] px-[18px]"
+				>
+					<NuxtImg
+						src="/google.svg"
+						width="28"
+						height="28"
+					/>
+					<p
+						class="text-start text-[12px] text-[#868C98]"
+					>
+						Google Reviews <br /><span
+							class="text-md text-black font-bold"
+							>4.7 / 152</span
+						>
+					</p>
+				</button>
+			</div>
+		</div>
+		<NuxtImg
+			src="/hero.png"
+			width="548"
+		/>
+	</section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* styles */
+</style>
